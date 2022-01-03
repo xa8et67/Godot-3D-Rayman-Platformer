4d7ccbf6583bf8d72dc29fb3008421c6073161d9
Jeremi Biernacki <jeremi360@gmail.com>
2022-01-03 19:08:31 +0100

    use gd-plgu to install addons

---
+++ plug.gd
@@ -0,0 +1,7 @@
+extends "res://addons/gd-plug/plug.gd"
+
+func _plugging():
+    # Declare your plugins in here with plug(src, args)
+    # By default, only "addons/" directory will be installed
+    plug("imjp94/gd-blender-3d-shortcuts")
+    plug("Calinou/kenney-prototype-textures")
